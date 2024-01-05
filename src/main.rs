50d5170d1e82a2ed65d35538088e821fc9408825
Ivan Ganev <iganev@cytec.bg>
2024-01-04 17:52:16 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use anyhow::Result;
+
+#[tokio::main]
+#[allow(unreachable_code)]
+async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
