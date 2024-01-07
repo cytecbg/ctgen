50e168cbb1f25da5ae09e523210ab45c3e3c1afb
Ivan Ganev <iganev@cytec.bg>
2024-01-06 20:55:55 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod cli;
 pub mod consts;
 pub mod error;
 pub mod profile;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,93 @@
 use anyhow::Result;
-use clap::Parser;
-use ctgen::cli::{Args, CommandConfig, Commands};
 use ctgen::consts::CONFIG_NAME_DEFAULT;
 use ctgen::profile::CtGenProfileConfigOverrides;
 use ctgen::CtGen;
 #[allow(unused_imports)]
 use log::{debug, error, info, log_enabled, Level};
+
+use clap::{Parser, Subcommand};
+use std::error::Error;
+
+#[derive(Parser, Debug)]
+#[command(author = "Cytec BG", version, about = "Code Template Generator", long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Manage code template config profiles
+    Config {
+        #[command(subcommand)]
+        op: CommandConfig,
+    },
+    /// Run code template generator
+    Run {
+        #[arg(long, default_value = "default")]
+        /// Config profile to use for this run
+        profile: Option<String>,
+
+        #[arg(long, conflicts_with = "dsn")]
+        /// Override profile env-file directive
+        env_file: Option<String>,
+
+        #[arg(long, conflicts_with = "dsn")]
+        /// Override profile env-var directive
+        env_var: Option<String>,
+
+        #[arg(long)]
+        /// Override profile DSN directive
+        dsn: Option<String>,
+
+        #[arg(long)]
+        /// Override profile target-dir directive
+        target_dir: Option<String>,
+
+        #[arg(long, value_parser = parse_prompt_key_val::<String, String>, number_of_values = 1)]
+        /// Prompt answer override, for example --prompt "dummy=1"
+        prompt: Option<Vec<(String, String)>>,
+
+        /// Database table name to generate code templates for
+        table: Option<String>,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+pub enum CommandConfig {
+    /// Add a config profile. If no name is given, template name from toml file will be used
+    Add {
+        #[arg(long, conflicts_with = "name")]
+        /// Add config as default
+        default: bool,
+        #[arg(long)]
+        /// Add config with specific name
+        name: Option<String>,
+
+        #[arg(default_value = ".")]
+        /// Path to Ctgen.toml file
+        path: String,
+    },
+    /// List all saved config profiles
+    List,
+    /// Remove a config profile
+    Rm {
+        /// Config profile name to remove
+        name: String,
+    },
+}
+
+pub fn parse_prompt_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
+    where
+        T: std::str::FromStr,
+        T::Err: Error + Send + Sync + 'static,
+        U: std::str::FromStr,
+        U::Err: Error + Send + Sync + 'static,
+{
+    let pos = s.find('=').ok_or_else(|| format!("invalid KEY=value: no `=` found in `{}`", s))?;
+
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+}
 
 #[tokio::main]
 #[allow(unreachable_code)]
@@ -49,9 +131,6 @@ async fn main() -> Result<()> {
         } => {
             let profile_name = if let Some(p) = profile.as_deref() { p } else { CONFIG_NAME_DEFAULT };
 
-            println!("run {} for {:?}", profile_name, table);
-            println!("prompts: {:?}", prompt);
-
             ctgen.set_current_profile(profile_name).await?;
 
             if env_file.is_some() || env_var.is_some() || dsn.is_some() || target_dir.is_some() {
@@ -66,7 +145,7 @@ async fn main() -> Result<()> {
 
             let profile = ctgen.get_current_profile().unwrap();
 
-            println!("using profile {}", profile.configuration().name());
+            //TODO
 
             Ok(())
         }
